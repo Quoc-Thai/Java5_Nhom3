1ad8f788012b578bc7e35d7513e3c818b137cdf6
Quoc-Thai <thainqpc04462@fpt.edu.vn>
2023-05-22 11:15:23 +0700

    Thai

---
+++ ASM_Java5_Nhom3/src/main/java/com/poly/demo/AsmJava5Nhom3Application.java
@@ -0,0 +1,13 @@
+package com.poly.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmJava5Nhom3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsmJava5Nhom3Application.class, args);
+	}
+
+}
