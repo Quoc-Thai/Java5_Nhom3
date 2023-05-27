350b33f329bcecc051cd14f3e723da91f72daad6
WeirdoJackDX <lhtho1803@gmail.com>
2023-05-27 23:11:27 +0700

    ASM1

--- Nhom3_ASM/src/main/java/com/poly/controller/SwitchPageController.java
+++ Nhom3_ASM/src/main/java/com/poly/controller/SwitchPageController.java
@@ -51,13 +51,14 @@ public class SwitchPageController {
 	public String Error() {
 		return "404";
 	}
+
 	@GetMapping("/login")
 	public String Login() {
 		return "login";
 	}
 
-	@ModelAttribute("products")
-	public List<SanPham> getPro() {
+	@ModelAttribute("productsA")
+	public List<SanPham> getBongTai() {
 		List<SanPham> list = new ArrayList<SanPham>();
 		SanPham sanPham1 = new SanPham(null, "Bông tai 1", 1000000.0, 800000.0, null, null,
 				"img/product/BongTai/bt1.png", null, null);
@@ -65,9 +66,50 @@ public class SwitchPageController {
 				"img/product/BongTai/bt2.png", null, null);
 		SanPham sanPham3 = new SanPham(null, "Bông tai 3", 2500000.0, 2000000.0, null, null,
 				"img/product/BongTai/bt3.png", null, null);
+		SanPham sanPham4 = new SanPham(null, "Bông tai 4", 2500000.0, 2000000.0, null, null,
+				"img/product/BongTai/bt4.png", null, null);
 		list.add(sanPham1);
 		list.add(sanPham2);
 		list.add(sanPham3);
+		list.add(sanPham4);
+		return list;
+	}
+
+	@ModelAttribute("productsB")
+	public List<SanPham> getDongHo() {
+		List<SanPham> list = new ArrayList<SanPham>();
+		SanPham sanPham1 = new SanPham(null, "Đồng Hồ 1", 1000000.0, 800000.0, null, null,
+				"img/product/DongHo/CalvinKlein/dh1.jpg", null, null);
+		SanPham sanPham2 = new SanPham(null, "Đồng Hồ 2", 1000000.0, 800000.0, null, null,
+				"img/product/DongHo/CalvinKlein/dh1.jpg", null, null);
+		list.add(sanPham1);
+		SanPham sanPham3 = new SanPham(null, "Đồng Hồ 3", 1000000.0, 800000.0, null, null,
+				"img/product/DongHo/JustCavalii/dh7.jpg", null, null);
+		list.add(sanPham1);
+		SanPham sanPham4 = new SanPham(null, "Đồng Hồ 4", 1000000.0, 800000.0, null, null,
+				"img/product/DongHo/JustCavalii/dh8.jpg", null, null);
+		list.add(sanPham1);
+		list.add(sanPham2);
+		list.add(sanPham3);
+		list.add(sanPham4);
+		return list;
+	}
+
+	@ModelAttribute("productsC")
+	public List<SanPham> getNhan() {
+		List<SanPham> list = new ArrayList<SanPham>();
+		SanPham sanPham1 = new SanPham(null, "Nhẫn 1", 1000000.0, 800000.0, null, null,
+				"img/product/NhanBac/anh1.png", null, null);
+		SanPham sanPham2 = new SanPham(null, "Nhẫn 2", 2500000.0, 2000000.0, null, null,
+				"img/product/NhacBac/anh2.png", null, null);
+		SanPham sanPham3 = new SanPham(null, "Nhẫn 3", 2500000.0, 2000000.0, null, null,
+				"img/product/NhanBac/anh3.png", null, null);
+		SanPham sanPham4 = new SanPham(null, "Nhẫn 4", 2500000.0, 2000000.0, null, null,
+				"img/product/NhanBac/anh4.png", null, null);
+		list.add(sanPham1);
+		list.add(sanPham2);
+		list.add(sanPham3);
+		list.add(sanPham4);
 		return list;
 	}
 }
