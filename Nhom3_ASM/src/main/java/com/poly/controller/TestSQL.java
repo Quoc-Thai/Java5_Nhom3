5b804ab49807d16383b2bfafc3d6ee5839e77177
tmco28 <tranmongco06@gmail.com>
2023-06-05 16:27:16 +0700

    5/6-16h26
    Test show sản phẩm

--- Nhom3_ASM/src/main/java/com/poly/controller/TestSQL.java
+++ Nhom3_ASM/src/main/java/com/poly/controller/TestSQL.java
@@ -1,10 +1,12 @@
 package com.poly.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.poly.DAO.SanPhamDAO;
 import com.poly.model.SanPham;
@@ -12,16 +14,23 @@ import com.poly.model.SanPham;
 @Controller
 public class TestSQL {
 	@Autowired
-	SanPhamDAO sanPhamDAO;
+	SanPhamDAO spdao;
 
-	@ResponseBody
-	@GetMapping("/sanpham")
-	public String index(Model model) {
-		var sanphams = sanPhamDAO.findAll();
-		for (SanPham sanPham : sanphams) {
-			var products = sanPham.getThuongHieu();
-			System.out.println(products.getTenTH());
-		}
-		return null;
+	
+	@GetMapping("sp")
+	public String sp() {
+		return "sanpham";
+		
 	}
+	
+	
+	@RequestMapping("sp")
+	public String index(Model model) {
+		SanPham item = new SanPham();
+		model.addAttribute("item", item);
+		List<SanPham> items = spdao.findAll();
+		model.addAttribute("items", items);
+		return "sanpham";
+	}
+	
 }
