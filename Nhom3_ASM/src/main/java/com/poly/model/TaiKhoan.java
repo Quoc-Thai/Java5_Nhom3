069b049b545777a481919ea7f049611ae81d6f5c
WeirdoJackDX <lhtho1803@gmail.com>
2023-06-05 16:22:35 +0700

    5/6 - 4:20
    Add Fragment

--- Nhom3_ASM/src/main/java/com/poly/model/TaiKhoan.java
+++ Nhom3_ASM/src/main/java/com/poly/model/TaiKhoan.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,11 +20,13 @@ import lombok.NoArgsConstructor;
 public class TaiKhoan {
 	@Id
 	@Column(name = "username")
+	@NotBlank(message = "Username không được bỏ trống")
 	String username;
+	@NotBlank(message = "Mật khẩu không được bỏ trống")
 	@Column(name = "password")
 	String password;
 	@Column(name = "is_admin")
-	Boolean isAdmin;
+	Boolean isAdmin = false;
 
 	@OneToMany(mappedBy = "taiKhoan")
 	List<HoaDon> hoaDon;
