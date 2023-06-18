fe405295eaf10a0f8023f77c309976ac8d65f2e8
tmco28 <tranmongco06@gmail.com>
2023-06-18 15:17:22 +0700

    18/6 15:20
    update admin

--- Nhom3_ASM/src/main/java/com/poly/controller/AdminController.java
+++ Nhom3_ASM/src/main/java/com/poly/controller/AdminController.java
@@ -1,19 +1,47 @@
 package com.poly.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
 
-	@GetMapping("/admin/index")
+	@GetMapping("/admin")
 	public String admin() {
-		return "admin/index";
+		return "admin_html/dashboard";
 	}
 
-	@GetMapping("/tables")
+	@GetMapping("/admin/table")
 	public String Table() {
-		return "admin/tables";
+		return "admin_html/basic-table";
+	}
+
+	@GetMapping("/admin/blank")
+	public String Blank() {
+		return "admin_html/blank";
+	}
+
+	@GetMapping("/admin/icon")
+	public String Icon() {
+		return "admin_html/fontawesome";
+	}
+
+	@GetMapping("/admin/profile")
+	public String Profile() {
+		return "admin_html/profile";
+	}
+
+	@GetMapping("/admin/product/add")
+	public String add() {
+		return "admin_html/addProduct";
+	}
+
+	@PostMapping("/admin/product/add/submit")
+	public String addSp(Model model) {
+		
+		return null;
 	}
 
 }
